eb80753c0221c03544e2b6d10b0a06307ea4dc1e
Roman Hood <roman.hood@adfitech.com>
2015-08-14 00:16:43 -0500

    simple script

---
+++ scripts/ad_hoc.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+def is_collection?(value)
+  return false unless value.is_a?(Array)
+  value.all?{ |x| x.is_a?(Hash) }
+end
+
+def clean hash
+  keys = hash.keys
+  p = Proc.new do |k,v|
+    if is_collection?(v)
+      v.each do |h|
+        h.delete_if(&p)
+      end
+    end
+    keys.include?(k)
+  end
+  hash.each do |k, v|
+    if is_collection?(v)
+      v.each do |obj|
+        obj.delete_if(&p)
+      end
+    end
+  end
+end
+
+data = {
+  "wholesaler":"US Foods",
+  "delivered":"2015-06-19T05:15:00-0500",
+  "contacts": [
+    {
+      "wholesaler":"US Foods",
+      "name":"John Lederer"
+    },
+    {
+      "wholesaler":"Sysco",
+      "name":"Bill Delaney"
+    }
+  ]
+}
+clean data
+puts data
